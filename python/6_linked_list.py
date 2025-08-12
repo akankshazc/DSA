2f350c2ffafff67573bf8591e603b2ff7d5d7788
Akanksha <akankshazc23@icloud.com>
2025-08-12 22:30:37 +0530

    Add docstrings and typing changes to make it more efficient

--- python/6_linked_list.py
+++ python/6_linked_list.py
@@ -1,33 +1,43 @@
-# Linked list implementation in Python
 
+"""
+Linked list implementation in Python.
+"""
+
+from typing import Any, Optional
 
 class Node:
-    # Creating a node
-    def __init__(self, item):
-        self.item = item
-        self.next = None
-
+    """
+    A node in a singly linked list.
+    """
+    def __init__(self, item: Any) -> None:
+        """
+        Initialize a node with an item and next pointer.
+        """
+        self.item: Any = item
+        self.next: Optional['Node'] = None
 
 class LinkedList:
-
-    def __init__(self):
-        self.head = None
+    """
+    A simple singly linked list.
+    """
+    def __init__(self) -> None:
+        """
+        Initialize an empty linked list.
+        """
+        self.head: Optional[Node] = None
 
 
 if __name__ == '__main__':
-
     linked_list = LinkedList()
-
     # Assign item values
     linked_list.head = Node(1)
     second = Node(2)
     third = Node(3)
-
     # Connect nodes
     linked_list.head.next = second
     second.next = third
-
-    # Print the linked list item
-    while linked_list.head != None:
-        print(linked_list.head.item, end=" ")
-        linked_list.head = linked_list.head.next
+    # Print the linked list items
+    current = linked_list.head
+    while current is not None:
+        print(current.item, end=" ")
+        current = current.next
