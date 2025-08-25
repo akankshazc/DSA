2791ebd6f1eb7a14754a3c84d7d250c7bd3ffbff
Akanksha <akankshazc23@icloud.com>
2025-08-25 06:53:24 +0530

    Refactor selection sort implementation for consistency and clarity; rename function and add type hints and docstrings

--- python/36_selection.py
+++ python/36_selection.py
@@ -1,24 +1,28 @@
-# Selection sort in Python
+from typing import List
+
+"""
+Selection Sort Algorithm in Python.
+Sorts a list in ascending order using selection sort.
+"""
 
 
-def selectionSort(array, size):
-   
-    for step in range(size):
+def selection_sort(array: List[int]) -> None:
+    """
+    Sorts the input list in ascending order using selection sort.
+    Args:
+        array (List[int]): List of integers to sort.
+    """
+    n = len(array)
+    for step in range(n):
         min_idx = step
-
-        for i in range(step + 1, size):
-         
-            # to sort in descending order, change > to < in this line
-            # select the minimum element in each loop
+        for i in range(step + 1, n):
             if array[i] < array[min_idx]:
                 min_idx = i
-         
-        # put min at the correct position
-        (array[step], array[min_idx]) = (array[min_idx], array[step])
+        array[step], array[min_idx] = array[min_idx], array[step]
 
 
-data = [-2, 45, 0, 11, -9]
-size = len(data)
-selectionSort(data, size)
-print('Sorted Array in Ascending Order:')
-print(data)
+if __name__ == "__main__":
+    data = [-2, 45, 0, 11, -9]
+    selection_sort(data)
+    print('Sorted Array in Ascending Order:')
+    print(data)
