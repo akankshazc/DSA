d982e82c9d00012f48c4a9c993757855dfc18382
Akanksha <akankshazc23@icloud.com>
2025-08-29 21:12:19 +0530

    Refactor bucket sort implementation for consistency and clarity; rename function and update docstrings

--- python/42_bucketsort.py
+++ python/42_bucketsort.py
@@ -1,31 +1,36 @@
-# Bucket Sort in Python
+
+"""
+Bucket Sort Algorithm in Python.
+Sorts a list of floating point numbers in [0, 1) using bucket sort.
+"""
+
+from typing import List
 
 
-def bucketSort(array):
-    bucket = []
-
-    # Create empty buckets
-    for i in range(len(array)):
-        bucket.append([])
-
-    # Insert elements into their respective buckets
-    for j in array:
-        index_b = int(10 * j)
-        bucket[index_b].append(j)
-
-    # Sort the elements of each bucket
-    for i in range(len(array)):
-        bucket[i] = sorted(bucket[i])
-
-    # Get the sorted elements
+def bucket_sort(array: List[float]) -> List[float]:
+    """
+    Sorts the input list of floats in ascending order using bucket sort.
+    Args:
+        array (List[float]): List of floats in [0, 1) to sort.
+    Returns:
+        List[float]: Sorted list.
+    """
+    n = len(array)
+    buckets: List[List[float]] = [[] for _ in range(n)]
+    for value in array:
+        index = int(n * value)
+        buckets[index].append(value)
+    for i in range(n):
+        buckets[i].sort()
     k = 0
-    for i in range(len(array)):
-        for j in range(len(bucket[i])):
-            array[k] = bucket[i][j]
+    for i in range(n):
+        for value in buckets[i]:
+            array[k] = value
             k += 1
     return array
 
 
-array = [.42, .32, .33, .52, .37, .47, .51]
-print("Sorted Array in descending order is")
-print(bucketSort(array))
+if __name__ == "__main__":
+    array = [.42, .32, .33, .52, .37, .47, .51]
+    print("Sorted Array in ascending order is")
+    print(bucket_sort(array))
