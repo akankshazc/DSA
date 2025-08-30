4484082eb1fdee9f889309a7211e18a138d8d953
Akanksha <akankshazc23@icloud.com>
2025-08-30 21:54:29 +0530

    Refactor Huffman coding implementation for consistency and clarity; improve type hints, enhance docstrings, and streamline code structure.

--- python/51_huffman.py
+++ python/51_huffman.py
@@ -1,12 +1,17 @@
-# Huffman Coding in python
+"""
+Huffman Coding in Python.
+Generates Huffman codes for characters in a string based on their frequencies.
+"""
 
-string = 'BCAADDDCCACACAC'
+from typing import Dict, Any
 
 
-# Creating tree nodes
-class NodeTree(object):
+class NodeTree:
+    """
+    Node for Huffman tree.
+    """
 
-    def __init__(self, left=None, right=None):
+    def __init__(self, left: Any = None, right: Any = None) -> None:
         self.left = left
         self.right = right
 
@@ -16,45 +21,47 @@ class NodeTree(object):
     def nodes(self):
         return (self.left, self.right)
 
-    def __str__(self):
-        return '%s_%s' % (self.left, self.right)
+    def __str__(self) -> str:
+        return f'{self.left}_{self.right}'
 
 
-# Main function implementing huffman coding
-def huffman_code_tree(node, left=True, binString=''):
-    if type(node) is str:
+def huffman_code_tree(node: Any, left: bool = True, binString: str = '') -> Dict[str, str]:
+    """
+    Recursively builds Huffman codes for each character.
+
+    Args:
+        node (Any): Current node or character.
+        left (bool): Direction flag.
+        binString (str): Current binary string.
+
+    Returns:
+        Dict[str, str]: Mapping of character to Huffman code.
+    """
+    if isinstance(node, str):
         return {node: binString}
-    (l, r) = node.children()
+    l, r = node.children()
     d = dict()
     d.update(huffman_code_tree(l, True, binString + '0'))
     d.update(huffman_code_tree(r, False, binString + '1'))
     return d
 
 
-# Calculating frequency
-freq = {}
-for c in string:
-    if c in freq:
-        freq[c] += 1
-    else:
-        freq[c] = 1
-
-freq = sorted(freq.items(), key=lambda x: x[1], reverse=True)
-
-nodes = freq
-
-while len(nodes) > 1:
-    (key1, c1) = nodes[-1]
-    (key2, c2) = nodes[-2]
-    nodes = nodes[:-2]
-    node = NodeTree(key1, key2)
-    nodes.append((node, c1 + c2))
-
-    nodes = sorted(nodes, key=lambda x: x[1], reverse=True)
-
-huffmanCode = huffman_code_tree(nodes[0][0])
-
-print(' Char | Huffman code ')
-print('----------------------')
-for (char, frequency) in freq:
-    print(' %-4r |%12s' % (char, huffmanCode[char]))
+if __name__ == "__main__":
+    string = 'BCAADDDCCACACAC'
+    freq = {}
+    for c in string:
+        freq[c] = freq.get(c, 0) + 1
+    freq = sorted(freq.items(), key=lambda x: x[1], reverse=True)
+    nodes = freq
+    while len(nodes) > 1:
+        (key1, c1) = nodes[-1]
+        (key2, c2) = nodes[-2]
+        nodes = nodes[:-2]
+        node = NodeTree(key1, key2)
+        nodes.append((node, c1 + c2))
+        nodes = sorted(nodes, key=lambda x: x[1], reverse=True)
+    huffmanCode = huffman_code_tree(nodes[0][0])
+    print(' Char | Huffman code ')
+    print('----------------------')
+    for (char, frequency) in freq:
+        print(f' {char!r:4} |{huffmanCode[char]:12}')
