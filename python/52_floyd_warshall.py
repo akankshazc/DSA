c4b305d180c8ca85b0bfbf970f57346c465a89a1
Akanksha <akankshazc23@icloud.com>
2025-08-31 22:40:10 +0530

    Refactor Floyd-Warshall algorithm implementation for consistency and clarity; enhance type hints, improve docstrings, and streamline code structure.

--- python/52_floyd_warshall.py
+++ python/52_floyd_warshall.py
@@ -1,37 +1,49 @@
-# Floyd Warshall Algorithm in python
+"""
+Floyd-Warshall Algorithm in Python.
+Finds shortest paths between all pairs of vertices in a weighted graph.
+"""
 
+from typing import List
 
-# The number of vertices
 nV = 4
-
 INF = 999
 
 
-# Algorithm implementation
-def floyd_warshall(G):
-    distance = list(map(lambda i: list(map(lambda j: j, i)), G))
-
-    # Adding vertices individually
+def floyd_warshall(G: List[List[int]]) -> None:
+    """
+    Runs Floyd-Warshall algorithm and prints shortest distances.
+    Args:
+        G (List[List[int]]): Adjacency matrix of the graph.
+    """
+    distance = [row[:] for row in G]
     for k in range(nV):
         for i in range(nV):
             for j in range(nV):
-                distance[i][j] = min(distance[i][j], distance[i][k] + distance[k][j])
+                distance[i][j] = min(
+                    distance[i][j], distance[i][k] + distance[k][j])
     print_solution(distance)
 
 
-# Printing the solution
-def print_solution(distance):
+def print_solution(distance: List[List[int]]) -> None:
+    """
+    Prints the distance matrix.
+    Args:
+        distance (List[List[int]]): Matrix of shortest distances.
+    """
     for i in range(nV):
         for j in range(nV):
-            if(distance[i][j] == INF):
+            if distance[i][j] == INF:
                 print("INF", end=" ")
             else:
                 print(distance[i][j], end="  ")
-        print(" ")
+        print()
 
 
-G = [[0, 3, INF, 5],
-         [2, 0, INF, 4],
-         [INF, 1, 0, INF],
-         [INF, INF, 2, 0]]
-floyd_warshall(G)
+if __name__ == "__main__":
+    G = [
+        [0, 3, INF, 5],
+        [2, 0, INF, 4],
+        [INF, 1, 0, INF],
+        [INF, INF, 2, 0]
+    ]
+    floyd_warshall(G)
